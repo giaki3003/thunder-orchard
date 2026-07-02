c41f12d2573a6d1af5437250c4f47c824c1a8dd0
Ash Manning <ash@layertwolabs.com>
2026-07-02 21:02:25 +0800

    tests: Use temp-dir

--- lib/state/two_way_peg_data.rs
+++ lib/state/two_way_peg_data.rs
@@ -1132,7 +1132,7 @@ pub fn disconnect(
 
 #[cfg(test)]
 mod test {
-    use std::{collections::BTreeMap, sync::Arc};
+    use std::collections::BTreeMap;
 
     use bitcoin::{
         Network,
@@ -1164,7 +1164,7 @@ mod test {
     // the failure must spend them again
     #[test]
     fn disconnect_failed_bundle_spends_reinstated_utxo() -> anyhow::Result<()> {
-        let (env, state) =
+        let (_temp_dir, env, state) =
             fresh_state("disconnect_failed_bundle_spends_reinstated_utxo")?;
         let outpoint = OutPoint::Regular {
             txid: Txid::from([1; 32]),
@@ -1226,7 +1226,7 @@ mod test {
     #[test]
     fn disconnect_withdrawal_event_block_uses_correct_db() -> anyhow::Result<()>
     {
-        let (env, state) =
+        let (_temp_dir, env, state) =
             fresh_state("disconnect_withdrawal_event_block_uses_correct_db")?;
 
         let block_height = 5u32;
@@ -1315,7 +1315,7 @@ mod test {
         >,
         f: impl FnOnce(&State, &mut sneed::RwTxn<'_>) -> R,
     ) -> anyhow::Result<R> {
-        let (env, state) = fresh_state(test_name)?;
+        let (_temp_dir, env, state) = fresh_state(test_name)?;
         let res = {
             let mut rwtxn = env.write_txn()?;
             state.height.put(
@@ -1352,10 +1352,6 @@ mod test {
             }
             f(&state, &mut rwtxn)
         };
-        drop(state);
-        let env_path = Arc::clone(env.path());
-        drop(env);
-        drop(std::fs::remove_dir_all(env_path));
         Ok(res)
     }
 
@@ -1395,7 +1391,7 @@ mod test {
     fn deposit_reorg_round_trips() -> anyhow::Result<()> {
         use crate::types::{Body, Header, proto::mainchain::Deposit};
 
-        let (env, state) = fresh_state("deposit_reorg_round_trips")?;
+        let (_temp_dir, env, state) = fresh_state("deposit_reorg_round_trips")?;
         let empty_body = Body {
             coinbase: Vec::new(),
             transactions: Vec::new(),
